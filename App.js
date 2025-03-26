9313b384df232c640197200aff098940a033cef2
PGX <snakekillah217@gmail.com>
2025-03-26 20:54:25 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express()
+const PORT = process.env.PORT || 3010;
+module.exports = app;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+async function search(name_movie) {
+ try {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${name_movie}&api_key=d8d6d57d6b54c98bd22d21d0130bd233`;
+  const response = await axios.get(url);
+  return response.data;
+ } catch (error) {
+  console.error('Error:', error);
+  return null;
+ }
+}
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'statics')));
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, 'views', 'search.html'));
+})
+
+app.get('/results', async (req, res) => {
+ const movieName = req.query.movie;
+ if (!movieName) {
+  return res.redirect('/');
+ }
+ const movies = await search(movieName);
+ res.render('results', {
+  movies,
+  movieName
+ });
+});
+
+if (require.main === module) {
+ app.listen(PORT, () => {
+   console.log(`SERVICIO ESCUCHANDO EN EL PUERTO => ${PORT}`);
+ });
+}
